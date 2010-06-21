e0b058be1c5aafb10e4b90e5b8e7af9d609b77de
gark87 <my_another@mail.ru>
2010-06-22 01:39:29 +0400

    folding/unfolding was added([-]/[+])

--- jira_code_helper.user.js
+++ jira_code_helper.user.js
@@ -1,6 +1,7 @@
 // ==UserScript==
 // @name           Jira {code} Helper
 // @namespace      http://gark_87.livejournal.com
+// @description    make work with JIRA {code} a little easier
 // @include        *jira*/browse/*-*
 // @version        0.0.1
 // ==/UserScript==
@@ -16,29 +17,60 @@ function keepBlanks(str) {
       });
 }
 
-var allCode;
-allCode = document.evaluate(
-        "//pre[@class='code-java']",
-	document,
-	null,
-	XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
-	null);
+// constants
+var fold   = '[-]';
+var unfold = '[+]';
+var uniqID = 'gark87';
+var monospace = 'font-family: monospace;';
 
-for (var i = 0; i < allCode.snapshotLength; i++) {
-      var thisCode = allCode.snapshotItem(i);
+// any <pre> tag
+var allPre = document.getElementsByTagName('pre');
+
+for (var i = 0; i < allPre.length; i++) {
+      var thisPre = allPre[i];
       var span = document.createElement('span');
-      span.setAttribute('style', 'font-family: monospace;');
-      var lines = thisCode.innerHTML.split(/[\r\n]/);
+      var id = uniqID + i;   // span id
+      span.setAttribute('id', id);
+      span.setAttribute('style', monospace);
+
+      // button to fold/unfold
+      var button = document.createElement('a');
+      button.innerHTML = unfold;
+
+      // code to fold/unfold
+      button.setAttribute('onClick', 
+         'if (this.innerHTML == "' + fold + '") {' +
+           'this.innerHTML = "' + unfold + '";' +
+           'document.getElementById("' + id + '").setAttribute("style", "' + 
+               monospace+'");'+
+         '} else {'+
+           'this.innerHTML = "' + fold + '";' +
+           'document.getElementById("' + id +
+               '").setAttribute("style", "display: none;");' +
+         '}');
+
+      // try to save format that was inside <pre>
+      var lines = thisPre.innerHTML.split(/[\r\n]/);
       for(var j = 0; j < lines.length; j++) {
-	var line = lines[j];
-	var len = line.length;
-	var result = '';
-	var nonBlank = 0;
-	for(; nonBlank < len && line.charAt(nonBlank) == ' '; nonBlank++)
-	  result += '&nbsp;';
-	span.innerHTML += result + 
-	  keepBlanks(line.substring(nonBlank, len)) + '<br/>';
+       var line = lines[j];
+       var len = line.length;
+       var result = '';
+       var nonBlank = 0;
+       for(; nonBlank < len && line.charAt(nonBlank) == ' '; nonBlank++)
+         result += '&nbsp;';
+       span.innerHTML += result + keepBlanks(line.substring(nonBlank, len)) + 
+         '<br />';
       }
-      thisCode.parentNode.insertBefore(span, thisCode);
-      thisCode.parentNode.removeChild(thisCode);
+
+      // insert <span>
+      thisPre.parentNode.insertBefore(span, thisPre);
+      // insert <a> button
+      thisPre.parentNode.parentNode.insertBefore(button, 
+         thisPre.parentNode);
+}
+
+// remove all <pre>
+for (var i = 0; i < allPre.length;) {
+      var thisPre = allPre[i];
+      thisPre.parentNode.removeChild(thisPre);
 }
