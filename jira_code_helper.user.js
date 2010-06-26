b855d365506fb044175f37a7d48b4ad2e277ab56
gark87 <my_another@mail.ru>
2010-06-26 05:10:26 +0400

    select source when unfold

--- jira_code_helper.user.js
+++ jira_code_helper.user.js
@@ -3,7 +3,7 @@
 // @namespace      http://gark_87.livejournal.com
 // @description    make work with JIRA {code} a little easier
 // @include        *jira*/browse/*-*
-// @version        0.0.2
+// @version        0.0.3
 // ==/UserScript==
 
 // this is about changing `   ' -> ` &nbsp;&nbsp;'
@@ -26,50 +26,56 @@ var uniqID = 'gark87';
 var allPre = document.getElementsByTagName('pre');
 
 for (var i = 0; i < allPre.length; i++) {
-      var thisPre = allPre[i];
-      var span = document.createElement('span');
-      var id = uniqID + i;   // span id
-      span.setAttribute('id', id);
-      span.setAttribute('style', 'display:none;');
+  var thisPre = allPre[i];
+  var span = document.createElement('span');
+  var id = uniqID + i;   // span id
+  span.setAttribute('id', id);
+  span.setAttribute('style', 'display:none;');
 
-      // button to fold/unfold
-      var button = document.createElement('a');
-      button.innerHTML = fold;
+  // button to fold/unfold
+  var button = document.createElement('a');
+  button.innerHTML = fold;
 
-      // code to fold/unfold
-      button.setAttribute('onClick', 
-         'if (this.innerHTML == "' + fold + '") {' +
-           'this.innerHTML = "' + unfold + '";' +
-           'document.getElementById("' + id + '").setAttribute("style", ' +
-	     '"font-family: monospace;");'+
-         '} else {'+
-           'this.innerHTML = "' + fold + '";' +
-           'document.getElementById("' + id +
-               '").setAttribute("style", "display: none;");' +
-         '}');
+  // code to fold/unfold
+  button.setAttribute('onClick',
+    'var code = document.getElementById("' + id + '");'+
+    'if (this.innerHTML == "' + fold + '") {' +
+      'this.innerHTML = "' + unfold + '";' +
+      'code.setAttribute("style", "font-family: monospace;");' +
+      'var range = document.createRange();' +
+      'range.selectNode(code);'+
+      'window.getSelection().removeAllRanges();'+
+      'setTimeout(function() {' +
+        'window.getSelection().addRange(range);' +
+      '}, 1);' +
+    '} else {'+
+      'this.innerHTML = "' + fold + '";' +
+      'code.setAttribute("style", "display: none;");' +
+    '}');
 
-      // try to save format that was inside <pre>
-      var lines = thisPre.innerHTML.split(/[\r\n]/);
-      for(var j = 0; j < lines.length; j++) {
-       var line = lines[j];
-       var len = line.length;
-       var result = '';
-       var nonBlank = 0;
-       for(; nonBlank < len && line.charAt(nonBlank) == ' '; nonBlank++)
-         result += '&nbsp;';
-       span.innerHTML += result + keepBlanks(line.substring(nonBlank, len)) + 
+  // try to save format that was inside <pre>
+  var lines = thisPre.innerHTML.split(/[\r\n]/);
+  for(var j = 0; j < lines.length; j++) {
+    var line = lines[j];
+    var len = line.length;
+    var result = '';
+    var nonBlank = 0;
+    for(; nonBlank < len && line.charAt(nonBlank) == ' '; nonBlank++)
+      result += '&nbsp;';
+    span.innerHTML += result +
+         keepBlanks(line.substring(nonBlank, len)) +
          '<br />';
-      }
+  }
 
-      // insert <span>
-      thisPre.parentNode.insertBefore(span, thisPre);
-      // insert <a> button
-      thisPre.parentNode.parentNode.insertBefore(button, 
-         thisPre.parentNode);
+  // insert <span>
+  thisPre.parentNode.insertBefore(span, thisPre);
+  // insert <a> button
+  thisPre.parentNode.parentNode.insertBefore(button,
+      thisPre.parentNode);
 }
 
 // remove all <pre>
 for (var i = 0; i < allPre.length;) {
-      var thisPre = allPre[i];
-      thisPre.parentNode.removeChild(thisPre);
+  var thisPre = allPre[i];
+  thisPre.parentNode.removeChild(thisPre);
 }
